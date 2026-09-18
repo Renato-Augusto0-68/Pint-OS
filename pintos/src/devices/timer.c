98acc297583d491d358fbb84145682225b9eb2eb
Renato-Augusto0-68 <rass@cesar.school>
2026-09-18 20:34:32 -0300

    thread alterations

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -89,19 +89,12 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 { 
-  static struct thread *idle_thread;
   int64_t start = timer_ticks ();
   ASSERT (intr_get_level () == INTR_ON);
   while (timer_elapsed (start) < ticks){
-     
-    if(thread_current()!=idle_thread){
-     printf("\nStart: %jd\n",start); thread_yield ();
-    }
-    printf("só para teste");
+     thread_yield ();
   }
-    
 }
-
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
    turned on. */
 void
@@ -177,7 +170,9 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
+
   thread_tick ();
+  
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
